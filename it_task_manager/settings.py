776bb0bfbe86a1b92435621400f9b7f97d995a4e
Oleksandr Merlenko <merlenko.business@gmail.com>
2025-01-29 18:20:17 +0100

    Add core models: Position, Worker, TaskType, Task and add AUTH_USER_MODEL to settings.py

--- it_task_manager/settings.py
+++ it_task_manager/settings.py
@@ -99,6 +99,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+AUTH_USER_MODEL = "task_manager.Worker"
 
 # Internationalization
 # https://docs.djangoproject.com/en/5.1/topics/i18n/
--- task_manager/models.py
+++ task_manager/models.py
@@ -1,3 +1,44 @@
+from django.conf import settings
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class Position(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+
+    def __str__(self):
+        return self.name
+
+class Worker(AbstractUser):
+    position = models.ForeignKey(Position, on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name = "worker"
+        verbose_name_plural = "workers"
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}, {self.position.name}"
+
+class TaskType(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+
+    def __str__(self):
+        return self.name
+
+class Task(models.Model):
+    class Priority(models.TextChoices):
+        URGENT = "Urgent", "Urgent"
+        HIGH = "High", "High"
+        MEDIUM = "Medium", "Medium"
+        LOW = "Low", "Low"
+
+    name = models.CharField(max_length=255)
+    description = models.TextField(blank=True)
+    deadline = models.DateTimeField()
+    is_completed = models.BooleanField(default=False)
+    priority = models.CharField(max_length=10, choices=Priority, default=Priority.MEDIUM)
+    task_type = models.ForeignKey(TaskType, on_delete=models.CASCADE)
+    assignees = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name="tasks")
+
+    def __str__(self):
+        return {self.name}
