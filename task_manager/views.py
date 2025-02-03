b744f419979d3272e72cef9f35593346a7436701
Oleksandr Merlenko <merlenko.business@gmail.com>
2025-02-03 18:08:04 +0100

    add basic index view

--- task_manager/views.py
+++ task_manager/views.py
@@ -1,3 +1,17 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+from task_manager.models import Worker
+
+
+def index(request) -> HttpResponse:
+    current_user = request.user
+    pending_tasks = current_user.tasks.filter(is_completed=False)
+    completed_tasks = current_user.tasks.filter(is_completed=True)
+
+    context = {
+        "pending_tasks": pending_tasks,
+        "completed_tasks": completed_tasks,
+    }
+
+    return render(request, "task_manager/index.html", context)
