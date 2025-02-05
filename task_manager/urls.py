2136df7b575e52e75729f22e931a0b2e93d00ac1
Oleksandr Merlenko <merlenko.business@gmail.com>
2025-02-05 19:45:26 +0100

    add tasks/ path to urlpatterns

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -1,9 +1,12 @@
+from asyncio import tasks
+
 from django.urls import path
 
-from task_manager.views import index
+from task_manager.views import index, TaskListView
 
 urlpatterns = [
     path("", index, name="index"),
+    path("tasks/", TaskListView.as_view(), name="tasks"),
 ]
 
 app_name = "task_manager"
