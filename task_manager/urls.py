735a73433ce6deafca23e01bcb24178ab573ccf0
Oleksandr Merlenko <merlenko.business@gmail.com>
2025-02-13 13:03:42 +0100

    add task create url to the urlpatterns

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -2,12 +2,13 @@ from asyncio import tasks
 
 from django.urls import path
 
-from task_manager.views import index, TaskListView, TaskDetailView
+from task_manager.views import index, TaskListView, TaskDetailView, TaskCreateView
 
 urlpatterns = [
     path("", index, name="index"),
     path("tasks/", TaskListView.as_view(), name="task-list"),
     path("tasks/<int:pk>/", TaskDetailView.as_view(), name="task-detail"),
+    path("tasks/create/", TaskCreateView.as_view(), name="task-create"),
 ]
 
 app_name = "task_manager"
