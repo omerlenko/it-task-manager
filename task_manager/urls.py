c1166a7c91a644d84c317ceaca1e908bf8ef522e
Oleksandr Merlenko <merlenko.business@gmail.com>
2025-02-13 13:30:16 +0100

    add task delete url to urlpatterns

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -2,7 +2,7 @@ from asyncio import tasks
 
 from django.urls import path
 
-from task_manager.views import index, TaskListView, TaskDetailView, TaskCreateView, TaskUpdateView
+from task_manager.views import index, TaskListView, TaskDetailView, TaskCreateView, TaskUpdateView, TaskDeleteView
 
 urlpatterns = [
     path("", index, name="index"),
@@ -10,6 +10,7 @@ urlpatterns = [
     path("tasks/<int:pk>/", TaskDetailView.as_view(), name="task-detail"),
     path("tasks/create/", TaskCreateView.as_view(), name="task-create"),
     path("tasks/<int:pk>/update/", TaskUpdateView.as_view(), name="task-update"),
+    path("tasks/<int:pk>/delete/", TaskDeleteView.as_view(), name="task-delete"),
 ]
 
 app_name = "task_manager"
