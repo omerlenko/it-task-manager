a9f9c9c279f2b6556818ab68b05e7da7301f71f9
Oleksandr Merlenko <merlenko.business@gmail.com>
2025-02-26 12:33:36 +0100

    register Tag model in admin panel

--- task_manager/admin.py
+++ task_manager/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 
-from task_manager.models import Task, TaskType, Position, Worker
+from task_manager.models import Task, TaskType, Position, Worker, Tag
 
 admin.site.register(TaskType)
 admin.site.register(Position)
 admin.site.register(Task)
 admin.site.register(Worker)
+admin.site.register(Tag)
