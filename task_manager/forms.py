323be64b54040e55ddd35229a1807846b91f6567
Oleksandr Merlenko <merlenko.business@gmail.com>
2025-02-28 12:26:21 +0100

    make tags field a TextInput field that creates and saves new tags on input or uses existing, add checkbox selection to assignees field

--- task_manager/forms.py
+++ task_manager/forms.py
@@ -1,17 +1,53 @@
 from django import forms
+from django.contrib.auth import get_user_model
 
-from task_manager.models import Task
+from task_manager.models import Task, Tag
 
 
 class TaskForm(forms.ModelForm):
+    assignees = forms.ModelMultipleChoiceField(
+        required=True,
+        queryset=get_user_model().objects.all(),
+        widget=forms.CheckboxSelectMultiple,
+    )
 
+    tags = forms.CharField(
+        required=False,
+        help_text="Enter tags separated by commas.",
+        widget=forms.TextInput(attrs={"placeholder": "e.g. bug, UI, backend"}),
+    )
 
     class Meta:
         model = Task
         fields = "__all__"
         widgets = {
-            "deadline": forms.DateTimeInput(attrs={"type": "date", "class": "form-control"}),
+            "deadline": forms.DateInput(attrs={"type": "date", "class": "form-control"}),
         }
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        if self.instance.pk:
+            tag_names = [tag.name for tag in self.instance.tags.all()]
+            self.initial["tags"] = ", ".join(tag_names)
+
+    def clean_tags(self):
+        tags = self.cleaned_data.get("tags", "")
+        tag_names = [tag.strip().replace(" ", "-").lower() for tag in tags.split(",") if tag.strip()]
+        return tag_names
+
+    def save(self, commit=True):
+        task = super().save(commit=False)
+        tag_names = self.cleaned_data["tags"]
+
+        if commit:
+            task.save()
+            task.assignees.set(self.cleaned_data["assignees"])
+
+            tags = [Tag.objects.get_or_create(name=name)[0] for name in tag_names]
+            task.tags.set(tags)
+
+        return task
 
 class TaskSearchForm(forms.Form):
     search = forms.CharField(
