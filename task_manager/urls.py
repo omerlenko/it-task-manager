d77e51b223997246ec7ce98f899695dc49a418db
Oleksandr Merlenko <merlenko.business@gmail.com>
2025-02-28 20:27:03 +0100

    add paths to register, profile, profile-update, worker-list, worker-detail pages

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -3,10 +3,15 @@ from asyncio import tasks
 from django.urls import path
 
 from task_manager.views import index, TaskListView, TaskDetailView, TaskCreateView, TaskUpdateView, TaskDeleteView, \
-    complete_task
+    complete_task, WorkerCreateView, WorkerUpdateView, WorkerDetailView, profile, WorkerListView
 
 urlpatterns = [
     path("", index, name="index"),
+    path("register/", WorkerCreateView.as_view(), name="register"),
+    path("profile/", profile, name="profile"),
+    path("profile/update/", WorkerUpdateView.as_view(), name="profile-update"),
+    path("workers/", WorkerListView.as_view(), name="worker-list"),
+    path("workers/<int:pk>/", WorkerDetailView.as_view(), name="worker-detail"),
     path("tasks/", TaskListView.as_view(), name="task-list"),
     path("tasks/<int:pk>/", TaskDetailView.as_view(), name="task-detail"),
     path("tasks/create/", TaskCreateView.as_view(), name="task-create"),
