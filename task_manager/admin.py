5d9998b6bc15dc7167d4cd04cebf75596142139d
Oleksandr Merlenko <merlenko.business@gmail.com>
2025-03-03 19:52:12 +0100

    register Team and Project models in admin panel

--- task_manager/admin.py
+++ task_manager/admin.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 
-from task_manager.models import Task, TaskType, Position, Worker, Tag
+from task_manager.models import Task, TaskType, Position, Worker, Tag, Team, Project
 
 admin.site.register(TaskType)
 admin.site.register(Position)
 admin.site.register(Task)
 admin.site.register(Worker)
 admin.site.register(Tag)
+admin.site.register(Team)
+admin.site.register(Project)
